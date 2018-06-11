6a96907b47a471044043eef0d8286487e993d980
Kieran <me@kieranajp.co.uk>
2018-06-12 00:31:58 +0200

    Add point to map

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -1,7 +1,39 @@
 mapboxgl.accessToken = 'tokentokentoken';
-var map = new mapboxgl.Map({
+
+const coords = [13.411975805000539, 52.53134099392974];
+
+const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/outdoors-v10?optimize=true',
-    center: [13.411975805000539, 52.53134099392974],
+    center: coords,
     zoom: 13
+});
+
+map.on('load', () => {
+    // Add a source and layer displaying a point which will be animated in a circle.
+    map.addSource('point', {
+        type: 'geojson',
+        data: {
+            type: 'Point',
+            coordinates: coords,
+        }
+    });
+
+    const points = [
+        { name: 'outline', radius: 13, color: '#ffffff' },
+        { name: 'color',   radius: 10, color: '#00ffcc' },
+        { name: 'dot',     radius: 4,  color: '#444444' },
+    ];
+
+    points.forEach(point => {
+        map.addLayer({
+            id: point.name,
+            source: 'point',
+            type: 'circle',
+            paint: {
+                'circle-radius': point.radius,
+                'circle-color': point.color
+            }
+        });
+    });
 });
